ebaf73f8f6c8b25f94bd9a6b311045bb36ba38da
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2017-11-09 11:32:36 -0500

    update photo links script

--- public/generate-photo-links.py
+++ public/generate-photo-links.py
@@ -1,9 +1,15 @@
 import json
 
-participants = json.load(open('./participants.json', 'r'))
-members = reduce(lambda x,y: x+y, map(lambda x: x['members'], participants))
+teams = json.load(open('./participants.json', 'r'))
 links = {}
-for member in members:
-	if member['username']:
-		links[member['username']] = ''
+for team in teams:
+	files = os.listdir('./teams/' + team['teamName'] + '/photos')
+	for member in team['members']:
+		if member['username']:
+			filtered_files = filter(lambda x: member['username'].toLower() == x.split('.')[0].toLower(), files)
+			if len(filtered_files) > 0:
+				links[member['username']] = filtered_files[0]
+			else:
+				links[member['username']] = ''
 json.dump(links, open('./photo-links.json', 'w'))
+
