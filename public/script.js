38ba058fe0e94404cd66a2ea43bfc5be3ff26dd4
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2017-11-09 16:59:57 +0000

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,21 +1,7 @@
 $(document).ready(function () {
   $.getJSON('./participants.json', function (teams) {
     $.getJSON('./photo-links.json', function (links) {
-  
-      /* Original (working) gallery container)
-      var getTeamContainer = function (teamName) {
-        return $('<div class="container">\
-          <div class="row">\
-            <div class="col-lg-12 text-center">\
-              <hr class="primary">\
-              <h2 class="section-heading">' + teamName + '</h2>\
-            </div>\
-          </div>\
-        </div>')
-      }
-      */
-      
-      /* New experimental */
+
       var getTeamContainer = function (teamName) {
         return $('<div class="container gallery-container">\
           <div class="row">\
@@ -27,31 +13,15 @@ $(document).ready(function () {
         </div>')
       }
       
-      var pathToPhoto = function (username, teamName) {
+      var getMemberColumn = function (name, username, teamName) {
+        var photoPath = './placeholder.png';
         if (username && links[username]) {
-          return './teams/' + teamName + '/photos/' + links[username];
+          photoPath = './teams/' + teamName + '/photos/' + links[username];
         }
-        return './placeholder.png';
-      }
-      
-      /* Original (working) individual person photo generator 
-      var getMemberColumn = function (name, username, teamName) {
-        return $('<div class="col-lg-3 col-md-6 text-center">\
-          <div class="service-box">\
-            <img width="200px" src="' + pathToPhoto(username, teamName) + '" alt="no photo">\
-            <h3>' + name + '</h3>\
-            <p>@' + username + '</p>\
-          </div>\
-        </div>');
-      }
-      */
-      
-      /* New Gallery (experimental) */
-      var getMemberColumn = function (name, username, teamName) {
         return $('<div class="col-sm-6 col-md-4">\
           <div class="thumbnail">\
             <a class="lightbox" href="ENTER LINK TO USER PROFILE">\
-            <img width="200px" src="' + pathToPhoto(username, teamName) + '" alt="no photo">\
+            <img width="200px" src="' + photoPath + '" alt="no photo">\
             </a>\
           </div>\
           <div class="caption">\
@@ -60,8 +30,8 @@ $(document).ready(function () {
           </div>\
         </div>');
       }
-                      
-      var addTeam = function (team) {
+      
+      teams.forEach(function (team) {
         var teamSection = $('<section></section>');
         var memberContainer = $('<div class="container"></div>')
         var memberRow = $('<div class="row"></div>');
@@ -73,10 +43,6 @@ $(document).ready(function () {
         teamSection.append(getTeamContainer(team['teamName']));
         teamSection.append(memberContainer);
         $('#teams').append(teamSection);
-      }
-  
-      teams.forEach(function (team) {
-        addTeam(team);
       });
     });
   });
