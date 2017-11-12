c6dd89c474e31675b02a77380d91f8e60cc01056
Amer Rez <arez@hawk.iit.edu>
2017-11-12 21:55:23 +0000

    Upload New File

---
+++ challenge1/app.py
@@ -0,0 +1,10 @@
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello from Heroku! To Infinity & Beyond team ", 200
